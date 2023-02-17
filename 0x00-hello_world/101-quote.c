a7a26be8dc53f8bb7fddab4faf1a51eac507b6f8
Blckhldr <bencoffie17@gmail.com>
2023-02-17 09:06:41 -0800

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error
- * Return 1 (Success)
+ *  main - Entry
+ * Return: 1
  */
 int main(void)
 {
-fput("and that peice of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
-return (1);
+	write91, "and that piece of art is useful\" - Dora Korpar, 2015-10-19, 59);
+	return (1);
 }
-~                                                                                                                                                                        
-~                
