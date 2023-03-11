552e377a52b8730723a7d59715485d9e1e12bf09
Blckhldr <bencoffie17@gmail.com>
2023-03-11 02:26:35 -0800

    4 update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,62 +1,70 @@
-#include<stdlib.h>
-#include<ctype.h>
-#include<string.h>
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
+ */
+int check_num(char *str)
+{
+	/*Declaring variables*/
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str)) /*count string*/
+
+	{
+		if (!isdigit(str[count])) /*check if str there are digit*/
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
+}
 
 /**
-*check_num-checkifastringcontainsonlydigits
-*@str:thestringtocheck
-*
-*Return:1ifthestringcontainsonlydigits,0otherwise
-*/
-intcheck_num(constchar*str)
-{
-size_tcount;
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
 
-for(count=0;count<strlen(str);count++)
-{
-if(!isdigit(str[count]))
-{
-return(0);
-}
-}
-return(1);
-}
+int main(int argc, char *argv[])
 
-/**
-*main-Entrypoint.Addsuptheintegersamongthecommand-linearguments.
-*@argc:thenumberofcommand-linearguments
-*@argv:anarrayofstringscontainingthecommand-linearguments
-*
-*Return:Always0.
-*/
-intmain(intargc,char*argv[])
 {
-intcount;
-intstr_to_int;
-intsum=0;
 
-if(argc<2)
-{
-printf("Error\n");
-return(1);
-}
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
+	int sum = 0;
 
-for(count=1;count<argc;count++)
-{
-if(check_num(argv[count]))
-{
-str_to_int=atoi(argv[count]);
-sum+=str_to_int;
-}
-else
-{
-printf("Error\n");
-return(1);
-}
-}
+	count = 1;
+	while (count < argc) /*Goes through the whole array*/
+	{
+		if (check_num(argv[count]))
 
-printf("%d\n",sum);
+		{
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+			sum += str_to_int;
+		}
 
-return(0);
+		/*Condition if one of the number contains symbols that are not digits*/
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		count++;
+	}
+
+	printf("%d\n", sum); /*print sum*/
+
+	return (0);
 }
